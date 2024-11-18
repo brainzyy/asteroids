c976c1dbbf4f67653b3745f9b9003be13246a3c5
brainzyy <br.stekelenburg@gmail.com>
2024-11-18 19:19:20 +0100

    implemented fps limitation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print ('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -15,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
