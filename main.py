4f1a3fe9db024c63cbfea105bf88f5e559097dfc
brainzyy <br.stekelenburg@gmail.com>
2024-11-19 18:06:55 +0100

    made groups

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ def main():
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
 
     while GAME_RUNNING is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for entity in updatable:
+            entity.update(dt)
         
         screen.fill("black")
-        player.update(dt)        
         
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+        
         dt = clock.tick(60) / 1000
-              
 
         
 
