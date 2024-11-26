a4c52d838f814fd1ada685edf1f806b2da2e553f
brainzyy <br.stekelenburg@gmail.com>
2024-11-26 21:46:05 +0100

    made asteroids and collision

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,18 @@
+import pygame
 from circleshape import CircleShape
+
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x 
+        self.y = y 
         
+    def draw(self, screen):
+        pygame.draw.circle(screen, 'white', (self.x, self.y), self.radius, 2)
+
+    def update(self, dt):
+        movement = self.velocity * dt
+        self.x += movement.x
+        self.y += movement.y
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -10,18 +12,31 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    updatable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
+    asteroid_field = AsteroidField()
 
     while GAME_RUNNING is True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        for entity in updatable:
+        for entity in updateable:
             entity.update(dt)
         
+        for asteroid in asteroids:
+            if player.collision(asteroid) is True: 
+                print ('Game over!')
+                sys.exit()
+
         screen.fill("black")
         
         for entity in drawable:
